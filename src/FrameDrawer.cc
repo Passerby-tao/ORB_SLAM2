a5ede26dc3ccb0da20bfc2f830eb17f1606317de
Lintao <username>
2022-04-04 22:38:48 +0200

    unsleep

--- src/FrameDrawer.cc
+++ src/FrameDrawer.cc
@@ -24,6 +24,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <unistd.h>  
+
 #include<mutex>
 
 namespace ORB_SLAM2
--- src/Optimizer.cc
+++ src/Optimizer.cc
@@ -19,7 +19,7 @@
 */
 
 #include "Optimizer.h"
-
+#include <unistd.h>  
 #include "Thirdparty/g2o/g2o/core/block_solver.h"
 #include "Thirdparty/g2o/g2o/core/optimization_algorithm_levenberg.h"
 #include "Thirdparty/g2o/g2o/solvers/linear_solver_eigen.h"
